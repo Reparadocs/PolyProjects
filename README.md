05c6ab0b21949e8d77a5018f93ce99cb7b696afe
Rishab Hegde <reparadocs@gmail.com>
2014-11-12 22:39:13 -0800

    Login should now work

--- README.md
+++ README.md
@@ -7,8 +7,7 @@ Get it working
 ==============
 
 1. Clone Project
-2. Add .env file with SECRET_KEY, SMTP_EMAIL, SMTP_PASSWORD, and ADDED_SECURITY_STRING
-3. Do something with heroku databse stuff in settings.py
+2. Add .env file with SECRET_KEY, SMTP_EMAIL, SMTP_PASSWORD
 4. Run syncdb
 5. Run init.py (Initializes fixtures)
 6. Run loaddata on each fixture (skills, categories, majors)
--- listings/views.py
+++ listings/views.py
@@ -19,8 +19,8 @@ def login(request):
     raise PermissionDenied
   poly_id = request.GET.get('ticket','')
   if poly_id == '':
-    return redirect('https://mydev.calpoly.edu/cas/login?service=https://mysterious-fortress-8708.herokuapp.com/login/')
-  r = requests.get('https://mydev.calpoly.edu/cas/validate?ticket='+poly_id+'&service=https://mysterious-fortress-8708.herokuapp.com/login/')
+    return redirect('https://my.calpoly.edu/cas/login?service=https://mypolyprojects.com/login/')
+  r = requests.get('https://my.calpoly.edu/cas/validate?ticket='+poly_id+'&service=https://mypolyprojects.com/login/')
   login_values = r.text.split()
   if login_values[0] == 'yes':
     try:
@@ -34,7 +34,7 @@ def login(request):
       auth_login(request, user)
       return redirect(reverse('listings.views.register'))
   else:
-    return redirect('https://mydev.calpoly.edu/cas/login?service=https://mysterious-fortress-8708.herokuapp.com/login/')
+    return redirect('https://my.calpoly.edu/cas/login?service=https://mypolyprojects.com/login/')
 
 def register(request):
   listings = Listing.objects.filter(owner=request.user)
